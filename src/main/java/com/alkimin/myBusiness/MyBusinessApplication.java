48c44e16afb59199479bcb6eaa77549109519fb0
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2024-03-05 11:04:32 -0300

    initial project

---
+++ src/main/java/com/alkimin/myBusiness/MyBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.alkimin.myBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkimin/myBusiness/MyBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkimin.myBusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
